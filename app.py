fc8b3761530dc82ca6b5271ba55e241b978771b6
uwais67 <114695768+uwais67@users.noreply.github.com>
2023-08-15 17:21:12 +0000

    main commit

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask , render_template, request
+from sklearn.preprocessing import StandardScaler
+import numpy as np
 import pickle
 app=Flask(__name__)
-clf=pickle.load(open("model/classifier.pkl",'rb'))
-
+classifier=pickle.load(open("model/classifier.pkl","rb"))
+scaler=pickle.load(open("model/scaler.pkl","rb"))
 @app.route('/', methods=["GET","POST"])
 def home():
       pred=None
@@ -15,8 +17,11 @@ def home():
         a6 = request.form.get('entry6')
         a7 = request.form.get('entry7')
         a8 = request.form.get('entry8')
-        pred=clf.predict([[a1,a2,a3,a4,a5,a6,a7,a8]])
-        pred=pred[0]
+        input_data_as_numpy_array = np.asarray((a1,a2,a3,a4,a5,a6,a7,a8))
+        input_data_reshaped = input_data_as_numpy_array.reshape(1,-1)
+        std_data = scaler.transform(input_data_reshaped)
+        pre=classifier.predict(std_data)
+        pred=pre[0]
       return render_template('index.html', pred=pred)
 
 if __name__=="__main__":
